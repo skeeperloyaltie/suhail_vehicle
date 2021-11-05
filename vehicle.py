3bc117de41c3320580760113bbe3b3223000faab
skeeperloyaltie <gugod254@gmail.com>
2021-11-05 19:02:53 +0300

    edited

--- vehicle.py
+++ vehicle.py
@@ -1,54 +1,47 @@
 ## Program Assessement
-
 # Class Vehicle
-
-
 class Vehicle:
     def __init__(self, fare):
         self.fare = fare
-
 # Part A create two instances of Vehicle,
- 
 class bus(Vehicle):
-    def __init__(self, fare, number_of_passengers):
-        self.fare = fare
-        self.number_of_passengers = number_of_passengers
-        
+    def __init__(self):
+        fare = Vehicle.fare
 class car(Vehicle):
-    def __init__(self, fare, number_of_passengers):
+    def __init__(self, fare):
         self.fare = fare
-        self.number_of_passengers = number_of_passengers
-class total:
-    def __add__(car, bus):
-        return car.fare + bus.fare
-class greaterthan:
-    def __gt__(self, bus, car):
-        return bus.fare > car.fare
 
-class lowerthan:
-    def __lt__(self, bus, car):
-        return bus.fare < car.fare  
-
-    
-
-
+# Use function __add__()
+def total(b, c):    
+    b = car.fare()
+    c = bus.fare()
+    def __add__(b, c):
+        return b + c
+# use function __gt__() and  __lt__()
+def instances(x, y):
+    x = car.fare 
+    y = bus.fare
+    def __gt__(x, y):
+        return x > y
+    def __lt__(x, y):
+        return x < y
+ 
 # Input fares
-
 if __name__ == '__main__':
     def main():
-        
         supermetro = Vehicle(1)
+        bus_fare = float(input("Enter Bus fare: ")) 
+        supermetro = bus(bus_fare)
+    # Nissans 
         nissan = Vehicle(1)
-        
-        fare  = float(input("Enter the fare: "))
-        number_of_passengers = int(input("Enter the number of passengers: "))
-        supermetro = bus(fare, number_of_passengers)
-        
-        print("The fare is: ", supermetro.fare)
-        print("The number of passengers is: ", supermetro.number_of_passengers) 
-        print("The total fare is: ", total(nissan, supermetro))
-
-
+        nissan_fare = float(input("Enter Nissans fare: ")) 
+        nissan = car(nissan_fare)
+    
+    # give the total of the two fares
+        print('Bus Fare is: $ {:.2f}'.format(bus_fare))
+        print('Car Fare is: $ {:.2f}'.format(nissan_fare))
+        print("Total fare is: ", total(bus_fare, nissan_fare))
+main()
 
        
     
